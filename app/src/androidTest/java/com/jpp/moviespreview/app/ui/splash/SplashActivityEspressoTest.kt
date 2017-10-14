b308875db61d88cb04e0e32ca217f6f127cd459c
Juan Peretti <jpp@Juans-MacBook-Pro.local>
2017-10-14 09:00:33 -0300

    Added mockito android

--- app/src/androidTest/java/com/jpp/moviespreview/app/ui/splash/SplashActivityEspressoTest.kt
+++ app/src/androidTest/java/com/jpp/moviespreview/app/ui/splash/SplashActivityEspressoTest.kt
@@ -2,6 +2,8 @@ package com.jpp.moviespreview.app.ui.splash
 
 import android.support.test.rule.ActivityTestRule
 import com.jpp.moviespreview.app.EspressoAppComponent
+import com.jpp.moviespreview.app.data.server.MoviesPreviewApi
+import com.jpp.moviespreview.app.mock
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Rule
@@ -15,10 +17,8 @@ class SplashActivityEspressoTest {
 
     @get:Rule val activityRule = ActivityTestRule(SplashActivity::class.java)
 
-//    private val appComponent by lazy {
-//        EspressoAppComponent
-//
-//    }
+
+    lateinit var mocked: MoviesPreviewApi
 
     @Before
     fun setUp() {
@@ -28,6 +28,7 @@ class SplashActivityEspressoTest {
 
     @Test
     fun someRamdomTest() {
+        mocked = mock()
         activityRule.launchActivity(null)
         Assert.assertEquals(1, 1)
     }
