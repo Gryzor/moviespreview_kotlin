f08a9b5b35805ed0f7d41020c2c2db72aabb1d4e
Juan Peretti <jpp@Juans-MacBook-Pro.local>
2017-10-20 16:19:59 -0300

    Movies database structure

---
+++ app/src/main/java/com/jpp/moviespreview/app/data/cache/db/MoviesDao.kt
@@ -0,0 +1,15 @@
+package com.jpp.moviespreview.app.data.cache.db
+
+import android.arch.persistence.room.Dao
+
+/**
+ * Dao representation for the Movies
+ *
+ * Manages the [MoviePage], [Movie] and [GenresByMovies] tables
+ *
+ * Created by jpp on 10/20/17.
+ */
+@Dao
+interface MoviesDao {
+    //TODO add methods
+}
