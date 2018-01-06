d2a77f7882fe2548b640497af35e7d485e7799ae
JPP <peretti.juan@gmail.com>
2018-01-06 16:21:03 -0300

    Ui for search implemented

--- app/src/main/java/com/jpp/moviespreview/app/ui/sections/main/search/SearchMoviesActivity.kt
+++ app/src/main/java/com/jpp/moviespreview/app/ui/sections/main/search/SearchMoviesActivity.kt
@@ -2,11 +2,8 @@ package com.jpp.moviespreview.app.ui.sections.main.search
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.SearchView
-import android.util.Log
-import android.view.Menu
-
 import com.jpp.moviespreview.R
+import kotlinx.android.synthetic.main.search_movies_activity.*
 
 /**
  * Created by jpp on 1/6/18.
@@ -15,16 +12,11 @@ class SearchMoviesActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.search_movies_activity)
+        setSupportActionBar(search_activity_toolbar)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        search_view.isIconified = false
+        search_view.setIconifiedByDefault(false)
     }
 
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.search_activity_menu, menu)
-        val searchItem = menu.findItem(R.id.action_search)
-        val searchView = searchItem.actionView as SearchView
-        searchView.isIconified = false
-        searchView.setIconifiedByDefault(false)
-        Log.d("Search", "Name -> ${searchView.id}")
-        return true
-    }
 }
