a2b9cce1d7192586207eda6d85e645571872166f
JPP <peretti.juan@gmail.com>
2018-03-28 16:23:03 -0300

    Added LicensesPresenterInteractor and tests

--- app/src/main/java/com/jpp/moviespreview/app/ui/sections/about/licenses/LicensesContract.kt
+++ app/src/main/java/com/jpp/moviespreview/app/ui/sections/about/licenses/LicensesContract.kt
@@ -1,6 +1,8 @@
 package com.jpp.moviespreview.app.ui.sections.about.licenses
 
+import com.jpp.moviespreview.app.ui.Error
 import com.jpp.moviespreview.app.ui.License
+import com.jpp.moviespreview.app.util.extentions.DelegatesExt
 
 /**
  * Created by jpp on 1/20/18.
@@ -14,4 +16,16 @@ interface LicensesView {
 interface LicensesPresenter {
     fun linkView(licencesView: LicensesView)
     fun onLicenseSelected(license: License)
+}
+
+interface LicensesPresenterInteractor {
+    fun retrieveLicenses(licensesData: LicensesData)
+}
+
+/**
+ * Defines a communication channel between presenter and interactor
+ */
+class LicensesData(onLicencesSetObserver: () -> Unit = {}) {
+    var licences: List<License>? by DelegatesExt.observerDelegate(onLicencesSetObserver)
+    var error: Error? by DelegatesExt.observerDelegate(onLicencesSetObserver)
 }
